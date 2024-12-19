ed3c3c402450a08819eacd1b99c7706b29ff8172
Ian Lucas <ianlucasdonado@gmail.com>
2024-12-19 18:31:55 -0500

    Ajoute configurations pour Base Swerve avec moteurs Mk4i_L3

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,16 +3,16 @@ package frc.robot;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.subsystems.MaxSwerveDrivetrain;
+import frc.robot.subsystems.KrakenMk4iDrivetrain;
 
 public class RobotContainer {
     private final CommandXboxController pilotGamepad = new CommandXboxController(0);
     @SuppressWarnings("unused")
-    // private final KrakenMk4Drivetrain drivetrain = new KrakenMk4Drivetrain(pilotGamepad);
-    private final MaxSwerveDrivetrain drivetrain = new MaxSwerveDrivetrain(pilotGamepad);
+    //private final KrakenMk4Drivetrain drivetrain = new KrakenMk4Drivetrain(pilotGamepad);
+    //private final MaxSwerveDrivetrain drivetrain = new MaxSwerveDrivetrain(pilotGamepad);
+    private final KrakenMk4iDrivetrain drivetrain = new KrakenMk4iDrivetrain(pilotGamepad);
 
-    public RobotContainer() {
-        pilotGamepad.a().whileTrue(drivetrain.steerAllWheelsAtRestTo(Rotation2d.fromDegrees(0)));
+    public RobotContainer() {       
         pilotGamepad.b().whileTrue(drivetrain.steerAllWheelsAtRestTo(Rotation2d.fromDegrees(15)));
     }
 
